866d7695ea1816143624f84acea0b7f34655b861
kyliewing <zijinzhao0529@gmail.com>
2016-10-15 12:37:00 -0700

    01

--- 01_TwoSum.py
+++ 01_TwoSum.py
@@ -1,3 +1,4 @@
+#Hash Table:O(n)
 class Solution(object):
     def twoSum(self,nums,target):
         if len(nums)<=1:
@@ -8,3 +9,15 @@ class Solution(object):
                 return [my_dict[nums[i]],i]
             else:
                 my_dict[target-nums[i]]=i
+
+#Your runtime beats 59.26% of python submissions.
+class Solution(object):
+    def twoSum(self, nums, target):
+        if len(nums)<=1:
+            return False
+        my_dict={}
+        for i in range(len(nums)):
+            if target-nums[i] in my_dict:
+                return [my_dict[target-nums[i]],i]
+            else:
+                my_dict[nums[i]]=i

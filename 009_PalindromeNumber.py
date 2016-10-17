e0951dc16a90a6c4528e53af9a47d4f5f8811591
kyliewing <zijinzhao0529@gmail.com>
2016-10-16 23:41:04 -0700

    9

--- 009_PalindromeNumber.py
+++ 009_PalindromeNumber.py
@@ -1,3 +1,5 @@
+#Determine whether an integer is a palindrome. Do this without extra space.
+
 #Your runtime beats 29.83% of python submissions.
 class Solution(object):
     def isPalindrome(self, x):
@@ -10,4 +12,23 @@ class Solution(object):
             i+=1
             j-=1
         return True
+        
+#Your runtime beats 59.25% of python submissions.
+#Use string
+class Solution(object):
+    def isPalindrome(self, x):
+        return str(x)==str(x)[::-1]
+
+#Determine whether an integer is a palindrome. Do this without extra space.
+#Your runtime beats 64.33% of python submissions.
+#Do not use string, use integer
+class Solution(object):
+    def isPalindrome(self, x):
+        orig,res=x,0
+        if x<0: return False
+        while x:
+            res=res*10+x%10
+            x/=10
+        return res==orig
+        
         

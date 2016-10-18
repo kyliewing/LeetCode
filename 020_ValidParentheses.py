a4462de86db3f9d46fb148694ac2a684bbb225a5
kyliewing <zijinzhao0529@gmail.com>
2016-10-18 14:58:16 -0700

    020

--- 020_ValidParentheses.py
+++ 020_ValidParentheses.py
@@ -1,18 +1,36 @@
-#Your runtime beats 85.35% of python submissions.
+#"Stack"
 #Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+#Your runtime beats 85.35% of python submissions.
+#My solution
 class Solution(object):
     def isValid(self, s):
-        stack_dict={'(':')', '{':'}', '[':']'}
-        stack_list=[]
-        for char in s:
-            if char=='(' or char =='{' or char=='[' or len(stack_list)==0:
+        stack_dict={'(':')', '{':'}', '[':']'}    #Usage of Hash Table
+        stack_list=[]                             
+        for char in s:  
+            if char=='(' or char =='{' or char=='[' or len(stack_list)==0:#If it's left bracket or empty, then append it
                 stack_list.append(char)
-            elif (stack_dict.has_key(stack_list[-1])) and char==stack_dict[stack_list[-1]]:
-                stack_list.pop()
+            elif (stack_dict.has_key(stack_list[-1])) and char==stack_dict[stack_list[-1]]:#If the last char has key and it's a pair
+                stack_list.pop()#The delete the last char
             else:
                 stack_list.append(char)
         if len(stack_list)==0:
             return True
         return False
+                
+        
+
+#Your runtime beats 60.58% of python submissions.
+#A simpler version: use dictionary and stack
+class Solution(object):
+    def isValid(self, s):
+        stackDict={')':'(', '}':'{', ']':'['}    #Usage of Hash Table
+        stack=[]                                 #Initialize the stack
+        for c in s:
+            if stack and c in stackDict and stack[-1]==stackDict[c]: #The condition that stack will pop one char,Notice it's == not =
+                stack.pop() 
+            else:
+                stack.append(c)                  #The condition that stack will push one char
+        return not stack
+        
                 
         

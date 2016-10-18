fc8dbf65ef33664ac1bc9e9c8960559f4cbae44a
kyliewing <zijinzhao0529@gmail.com>
2016-10-17 21:13:21 -0700

    014

--- 014_LongestCommonPrefix.py
+++ 014_LongestCommonPrefix.py
@@ -17,3 +17,29 @@ class Solution(object):
                 break
         return res
         
+
+#Your runtime beats 50.21% of python submissions.
+#"Already implemented in python" version: python library
+class Solution(object):
+    import os
+    def longestCommonPrefix(self, strs):
+        return os.path.commonprefix(strs)
+
+
+#Your runtime beats 60.60% of python submissions.
+#"Zip" version: 
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        res=""
+        for cs in zip(*strs):    #unzip strs, then deal with every char tuple
+            if len(set(cs))==1:  #if the char is unique in the tuple, notice the usesage of set 
+                res+=cs[0]
+            else:
+                break
+        return res
+        
+        
+        
+
+
+        

10cdf36eff50e5eae5fc5865ba4e656211fbfff6
kyliewing <zijinzhao0529@gmail.com>
2016-10-17 17:24:27 -0700

    013

--- 013_RomanToInteger.py
+++ 013_RomanToInteger.py
@@ -23,6 +23,46 @@ class Solution(object):
         if roman_dict[s[-2]]>=roman_dict[s[-1]]:# don't miss the last single symbol
             res+=roman_dict[s[-1]]
         return res
+
+#Your runtime beats 58.58% of python submissions.
+#Backwards make the rule easier, usage of dictionary map
+class Solution(object):
+    def romanToInt(self, s): 
+        roman_dict={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}  #Dictionary to sotre pairs of symbols and values
+        res,length=0,len(s)
+        if length==0: return 0
+        if length==1: return roman_dict[s]
+        res+=roman_dict[s[-1]]
+        for i in xrange(length-2,-1,-1):  #Instead of upwards, we use backwards and can make the rule easier
+            if roman_dict[s[i]]>=roman_dict[s[i+1]]:
+                res+=roman_dict[s[i]]
+            else:
+                res-=roman_dict[s[i]]
+        return res
+
+#Your runtime beats 7.51% of python submissions.
+#A more concise solution
+class Solution(object):
+    def romanToInt(self, s): 
+        roman_dict={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}  #Dictionary to sotre pairs of symbols and values
+        res,pre=0,'I'
+        for c in s[::-1]:  #Instead of upwards, we use backwards and can make the rule easier
+            res,pre=res+roman_dict[c] if roman_dict[c]>=roman_dict[pre] else res-roman_dict[c],c
+        return res
+        
+                
+            
+            
+            
+        
+        
+        
+                
+            
+            
+            
+        
+        
         
                 
             

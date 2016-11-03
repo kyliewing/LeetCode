12361c62ee101d853796a4b424183cd4d703735c
kyliewing <zijinzhao0529@gmail.com>
2016-11-02 23:56:56 -0700

    125

--- 125_ValidPalindorme.py
+++ 125_ValidPalindorme.py
@@ -25,5 +25,44 @@ class Solution(object):
         return True
             
         
+#Better
+#Your runtime beats 96.53% of python submissions.
+class Solution(object):
+    def isPalindrome(self, s):
+        s=s.lower().strip()
+        if len(s)==0: return True
+        head,tail=0,len(s)-1
+        while head<tail:
+            if not (s[head].isalnum()): #The usage of string.isalnum()
+                head+=1
+                continue
+            if not (s[tail].isalnum()): 
+                tail-=1
+                continue
+            if s[head]!=s[tail]: return False
+            head,tail=head+1,tail-1
+        return True
+         
+            
+#Build in function version
+#Your runtime beats 78.80% of python submissions.
+class Solution(object):
+    def isPalindrome(self, s):
+        s=[c.lower() for c in s if c.isalnum()] #Do not miss s.lower()
+        return s==s[::-1]
+         
+            
 
+#Regex Version
+#Your runtime beats 78.80% of python submissions.
+import re
+class Solution(object):
+    def isPalindrome(self, s):
+        s=re.sub("[^A-Za-z0-9]", "",s).lower()#Do not miss lower
+        return s==s[::-1]
+         
+            
+        
+        
+        
        
